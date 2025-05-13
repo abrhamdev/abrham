e679343f142b6e3b24bd90831380bf841a39b2a3
Abrham <169100762+abrhamdev@users.noreply.github.com>
2025-05-13 23:01:31 +0300

    Update multer.js

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 import path from 'path';
 
 const storage = multer.diskStorage({
-  destination: '/uploads/',
+  destination: 'uploads/',
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
     cb(null, `${file.fieldname}-${Date.now()}${ext}`);
